81771ef5b3a107c3fac80ccf178a51dc49560c97
bleak.twig <bruno.benkel@gmail.com>
2021-10-08 09:53:10 -0300

    Slightly updated run script

--- run.sh
+++ run.sh
@@ -2,7 +2,10 @@
 
 # Setup
 CLASGROOVY="/home/twig/data/code/jsw/coatjava-5.6.8/bin/run-groovy"
-FILE="/home/twig/data/code/jsw/recon_data/out_clas_012016.hipo"
+
+i="0"
+args=""
+for arg; do args="$args $arg"; done
 
 # Run
-$CLASGROOVY acceptance.groovy $FILE
+$CLASGROOVY "acceptance.groovy$args"
