2fcca072107996e92ff7f28caa6d112fd43910b8
Bruno Benkel <bruno.benkel@gmail.com>
2022-01-13 11:46:19 -0300

    Updated run script

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/sh -f
 
 # Setup
-CLASGROOVY="/home/twig/data/code/jsw/coatjava-5.6.8/bin/run-groovy"
-
 i="0"
 args=""
 for arg; do args="$args $arg"; done
 
 # Run
-$CLASGROOVY "acceptance.groovy$args"
+run-groovy "acceptance.groovy$args"
