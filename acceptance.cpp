826661233841d2764f5ceb089d37813a9523156e
Bruno Benkel <bruno.benkel@gmail.com>
2022-01-19 12:43:04 -0300

    Implemented function to get run number from input file name

--- acceptance.cpp
+++ acceptance.cpp
@@ -6,6 +6,15 @@
 // #include <map>
 // #include <string>
 
+int get_run_no(char* input_file) {
+    char* run_no_str = (char*) malloc(7);
+    strncpy(run_no_str, input_file + ((strchr(input_file, '.')) - input_file) - 6, 6);
+    int run_no_int = atoi(run_no_str);
+    free(run_no_str);
+
+    return run_no_int;
+}
+
 int main(int argc, char** argv) {
     // Handle optional arguments.
     bool use_fmt = false;
@@ -30,14 +39,24 @@ int main(int argc, char** argv) {
     char* input_file = (char*) malloc(strlen(argv[argc - 1]) + 1);
     strcpy(input_file, argv[argc - 1]);
 
-    if (!strstr(input_file, ".hipo")) {
+    if (!strstr(input_file, ".hipo")) { // Check that file is valid.
         fprintf(stderr, "ERROR. input_file should be a hipo file. Currently is: %s.\n", input_file);
         return 1;
     }
+    if (!access(input_file, F_OK) == 0) { // Check that file exists.
+        fprintf(stderr, "ERROR. %s does not exist!\n", input_file);
+        return 1;
+    }
+    int run_no = get_run_no(input_file);
+    if (run_no == 0) { // Check that run number was extracted correctly.
+        fprintf(stderr, "ERROR. Run number could not be extracted from input file %s.", input_file);
+        return 1;
+    }
 
-    printf("use_fmt: %d\n", use_fmt);
-    printf("nevents: %d\n", nevents);
+    printf("use_fmt:    %d\n", use_fmt);
+    printf("nevents:    %d\n", nevents);
     printf("input_file: %s\n", input_file);
+    printf("run_no:     %d\n", run_no);
 
     free(input_file);
     return 0;
