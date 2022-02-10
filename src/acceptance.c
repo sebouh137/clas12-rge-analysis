eef843bc44ad147acc065f79f4fcde149ceaec23
Bruno Benkel <bruno.benkel@gmail.com>
2022-02-10 10:14:21 -0300

    Added maps to the mix

--- src/acceptance.c
+++ src/acceptance.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <map>
 
 #include <TFile.h>
 
@@ -10,16 +11,40 @@
 #include "file_handler.h"
 #include "io_handler.h"
 
-int run(char *input_file, bool use_fmt, int nevets, int run_no, double beam_energy) {
+int run(char *input_file, bool use_fmt, int nevents, int run_no, double beam_energy) {
     // Create output file.
     TFile f("out/histos.root", "RECREATE");
 
-    // Open input file.
-    BankHist bankDraw(input_file);
+    // Open relevant data banks.
+    // std::map<char*, std::map<char*, BankHist>> banks;
+    std::map<char const *, BankHist *> banks = {
+            {"REC::Particle", new BankHist(input_file)},
+            // {"", new BankHist(input_file)},
+            // {"", new BankHist(input_file)},
+            // {"", new BankHist(input_file)},
+            // {"", new BankHist(input_file)},
+            // {"", new BankHist(input_file)},
+    };
 
-    // Draw some shit.
-    bankDraw.Hist1D("REC::Particle::Pz", 100, 0, 10, "");
-    bankDraw.Draw();
+    banks["REC::Particle"]->SetEntries(nevents);
+
+    // BankHist rec_part(input_file);
+    // BankHist rec_trk (input_file);
+    // BankHist rec_traj(input_file);
+    // BankHist fmt_trks(input_file);
+    // BankHist rec_ecal(input_file);
+    // BankHist rec_tof (input_file);
+    // if (nevents != 0) {
+    //     rec_part.SetEntries(nevents);
+    //     rec_trk .SetEntries(nevents);
+    //     rec_traj.SetEntries(nevents);
+    //     fmt_trks.SetEntries(nevents);
+    //     rec_ecal.SetEntries(nevents);
+    //     rec_tof .SetEntries(nevents);
+    // }
+
+    banks["REC::Particle"]->Hist1D("REC::Particle::Pz", 100, 0, 12, "");
+    banks["REC::Particle"]->Draw();
 
     // Write into file.
     f.Write();
@@ -29,7 +54,7 @@ int run(char *input_file, bool use_fmt, int nevets, int run_no, double beam_ener
     return 0;
 }
 
-// Execute program from CLAS12ROOT (`.x src/acceptance.c(filename, use_fmt, nevents)`).
+// Execute program from clas12root (`.x src/acceptance.c(filename, use_fmt, nevents)`).
 int acceptance(char *input_file, bool use_fmt, int nevents) {
     int    run_no      = -1;
     double beam_energy = -1;
