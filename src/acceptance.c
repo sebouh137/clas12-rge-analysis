7a6e5f088e796fcb39a858d5a5bcb1f65fe1431e
Bruno Benkel <bruno.benkel@gmail.com>
2022-03-14 16:20:12 -0300

    Added constants header file

--- src/acceptance.c
+++ src/acceptance.c
@@ -8,69 +8,10 @@
 
 #include <BankHist.h>
 
+#include "constants.h"
 #include "err_handler.h"
 #include "file_handler.h"
 #include "io_handler.h"
-
-// TODO. These #defines should be in their own file.
-// Masses.
-#define PIMASS  0.139570 // Pion mass.
-#define PRTMASS 0.938272 // Proton mass.
-#define NTRMASS 0.939565 // Neutron mass.
-#define EMASS   0.000051 // Electron mass.
-
-// Particle map keys.
-#define PALL "All particles"
-#define PPOS "Positive particles"
-#define PNEG "Negative particles"
-#define PPIP "Pi+"
-#define PPIM "Pi-"
-#define PELC "e-"
-#define PTRE "Trigger e-"
-
-// Histogram map keys.
-#define VZ       "Vz"
-#define VZPHI    "Vz vs phi"
-#define VZTHETA  "Vz vs theta"
-
-#define VP       "Vp"
-#define BETA     "Beta"
-#define BETAVP   "Beta vs Vp"
-
-#define DTOF     "TOF Difference"
-#define VPTOF    "Vp vs TOF Difference"
-
-#define PDIVEP   "Vp/E vs Vp"
-#define PDIVEE   "Vp/E vs E"
-#define PPCALE   "Vp vs E (PCAL)"
-#define PECINE   "Vp vs E (ECIN)"
-#define PECOUE   "Vp vs E (ECOU)"
-#define ECALPCAL "E (ECAL) vs E (PCAL)"
-#define PCALSF   "PCAL Sampling Fraction s"
-#define PCALSF1  "PCAL Sampling Fraction s1"
-#define PCALSF2  "PCAL Sampling Fraction s2"
-#define PCALSF3  "PCAL Sampling Fraction s3"
-#define PCALSF4  "PCAL Sampling Fraction s4"
-#define PCALSF5  "PCAL Sampling Fraction s5"
-#define PCALSF6  "PCAL Sampling Fraction s6"
-#define ECINSF   "ECIN Sampling Fraction s"
-#define ECINSF1  "ECIN Sampling Fraction s1"
-#define ECINSF2  "ECIN Sampling Fraction s2"
-#define ECINSF3  "ECIN Sampling Fraction s3"
-#define ECINSF4  "ECIN Sampling Fraction s4"
-#define ECINSF5  "ECIN Sampling Fraction s5"
-#define ECINSF6  "ECIN Sampling Fraction s6"
-#define ECOUSF   "ECOU Sampling Fraction s"
-#define ECOUSF1  "ECOU Sampling Fraction s1"
-#define ECOUSF2  "ECOU Sampling Fraction s2"
-#define ECOUSF3  "ECOU Sampling Fraction s3"
-#define ECOUSF4  "ECOU Sampling Fraction s4"
-#define ECOUSF5  "ECOU Sampling Fraction s5"
-#define ECOUSF6  "ECOU Sampling Fraction s6"
-
-#define Q2       "Q2"
-#define NU       "nu"
-#define XB       "X_bjorken"
 
 double to_deg(double radians) {return radians * (180.0 / M_PI);}
 double calc_Q2(double beam_E, double momentum, double theta) {
