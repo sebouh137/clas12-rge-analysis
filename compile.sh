c8c50f5d1e82285990d948926381744e1f136a8d
Bruno Benkel <bruno.benkel@gmail.com>
2022-03-16 17:07:53 -0300

    Deleted outdated compile script

--- compile.sh
+++
@@ -1,4 +0,0 @@
-# /bin/sh -f
-g++ -g -o main src/acceptance.c src/err_handler.c src/file_handler.c src/io_handler.c -L libClas12Root.so `root-config --cflags --libs`
-
-# NOTE. This should be made with makefile you bozo.
