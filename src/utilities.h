18494ad1490d44b115c52b75796056cb1fd35eb4
Bruno Benkel <bruno.benkel@gmail.com>
2022-03-16 12:26:42 -0300

    Separated utilities.h into source code and header file

--- src/utilities.h
+++ src/utilities.h
@@ -3,15 +3,11 @@
 
 #include <math.h>
 
-double to_deg(double radians) {return radians * (180.0 / M_PI);}
-double calc_Q2(double beam_E, double momentum, double theta) {
-    return 4 * beam_E * momentum * pow(sin(theta/2), 2);
-}
-double calc_nu(double beam_E, double momentum) {
-    return beam_E - momentum;
-}
-double calc_Xb(double beam_E, double momentum, double theta) {
-    return (calc_Q2(beam_E, momentum, theta)/2) / (calc_nu(beam_E, momentum)/PRTMASS);
-}
+#include "constants.h"
+
+double to_deg(double radians);
+double calc_Q2(double beam_E, double momentum, double theta);
+double calc_nu(double beam_E, double momentum);
+double calc_Xb(double beam_E, double momentum, double theta);
 
 #endif
