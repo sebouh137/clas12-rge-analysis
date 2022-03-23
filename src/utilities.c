af36f35450415fc9840a02ecfd2a0efe56167fbb
Bruno Benkel <bruno.benkel@gmail.com>
2022-03-23 14:38:08 -0300

    Added some utils for recon

--- src/utilities.c
+++ src/utilities.c
@@ -1,6 +1,17 @@
 #include "utilities.h"
 
-double to_deg(double radians) {return radians * (180.0 / M_PI);}
+double to_deg(double radians) {
+    return radians * (180.0 / M_PI);
+}
+double calc_theta(double px, double py, double pz) {
+    return px == 0.0 && py == 0.0 && pz == 0.0 ? 0.0 : atan2(sqrt(px*px + py*py), pz);
+}
+double calc_phi(double px, double py) {
+    return atan2(py, px);
+}
+double calc_P(double px, double py, double pz) {
+    return sqrt(px*px + py*py + pz*pz);
+}
 double calc_Q2(double beam_E, double momentum, double theta) {
     return 4 * beam_E * momentum * pow(sin(theta/2), 2);
 }
--- src/utilities.h
+++ src/utilities.h
@@ -6,6 +6,9 @@
 #include "constants.h"
 
 double to_deg(double radians);
+double calc_theta(double px, double py, double pz);
+double calc_phi(double px, double py);
+double calc_P(double px, double py, double pz);
 double calc_Q2(double beam_E, double momentum, double theta);
 double calc_nu(double beam_E, double momentum);
 double calc_Xb(double beam_E, double momentum, double theta);
