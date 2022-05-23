292fdf3d6dacd1acab30e961312df28613107478
Bruno Benkel <bruno.benkel@gmail.com>
2022-05-23 11:44:16 -0400

    Added scanf for var

--- src/draw_plots.c
+++ src/draw_plots.c
@@ -34,12 +34,12 @@ int run() {
 
     // Check if we are to make a 1D or 2D plot.
     int plt_n =  2;
-    int px    = -1;
     const char * plt_str[plt_n] = {R_PLOT1D, R_PLOT2D};
+    int px    = -1;
 
     while (true) {
-        char plt_type[7];
-        printf("Plot type? [");
+        char plt_type[3];
+        printf("\nPlot type? [");
         for (int pi = 0; pi < plt_n; ++pi) printf("%s, ", plt_str[pi]);
         printf("\b\b]:\n>>> ");
         scanf("%6s", plt_type);
@@ -48,36 +48,36 @@ int run() {
         if (px != -1) break;
     }
 
-    // for (int pi = 0; pi < plt_n; ++pi) {
-    //     regex_t plt_rx;
-    //     if (regcomp(&plt_rx, plt_str[pi], 0)) return 1;
-    //     if (regexec(&plt_rx, command, 0, NULL, 0) == 0) {
-    //         if (px != -1) return 2;
-    //         px = pi;
-    //     }
-    // }
-    // if (px == -1) return 3;
-    // printf("Plot type is: %d\n", px);
-
     // Check variable(s) to be plotted.
-    // int n_vars = 24;
-    // const char * var_str[n_vars] = {R_RUNNO, R_EVENTNO, R_BEAME, R_PID, R_CHARGE, R_MASS, R_VX,
-    //         R_VY, R_VZ, R_PX, R_PY, R_PZ, R_P, R_THETA, R_PHI, R_BETA, R_PCAL_E, R_ECIN_E, R_ECOU_E,
-    //         R_TOT_E, R_DTOF, R_Q2, R_NU, R_XB, R_W2};
-    // int vx = -1;
-    // int vy = -1;
-    // for (int vi = 0; vi < n_vars; ++vi) {
-    //     regex_t var_rx;
-    //     if (regcomp(&var_rx, var_str[vi], 0)) return 4;
-    //     if (regexec(&var_rx, command, 0, NULL, 0) == 0) {
-    //         if      (vx == -1) vx = vi;
-    //         else if (vy == -1) vy = vi;
-    //         else               return 5;
-    //     }
-    // }
-    // if (px == 0 && (vx == -1 || vy != -1)) return 6;
-    // if (px == 1 && (vx == -1 || vy == -1)) return 6;
-    // printf("Vars are: %d, %d\n", vx, vy);
+    int n_vars = 24;
+    const char * var_str[n_vars] = {R_RUNNO, R_EVENTNO, R_BEAME, R_PID, R_CHARGE, R_MASS, R_VX,
+            R_VY, R_VZ, R_PX, R_PY, R_PZ, R_P, R_THETA, R_PHI, R_BETA, R_PCAL_E, R_ECIN_E, R_ECOU_E,
+            R_TOT_E, R_DTOF, R_Q2, R_NU, R_XB, R_W2};
+    int vx = -1;
+    int vy = -1;
+
+    while (true) {
+        char varx[32];
+        printf("\nDefine variable to be plotted on the x axis. Available variables:\n[");
+        for (int vi = 0; vi < n_vars; ++vi) printf("%s, ", var_str[vi]);
+        printf("\b\b]\n>>> ");
+        scanf("%31s", varx);
+
+        for (int vi = 0; vi < n_vars; ++vi) if (!strcmp(varx, var_str[vi])) vx = vi;
+        if (vx != -1) break;
+    }
+    if (px == 1) {
+        while (true) {
+            char vary[32];
+            printf("\nDefine variable to be plotted on the y axis. Available variables:\n[");
+            for (int vi = 0; vi < n_vars; ++vi) printf("%s, ", var_str[vi]);
+            printf("\b\b]\n>>> ");
+            scanf("%31s", vary);
+
+            for (int vi = 0; vi < n_vars; ++vi) if (!strcmp(varx, var_str[vi])) vy = vi;
+            if (vy != -1) break;
+        }
+    }
 
     // TODO. Define ranges.
     // double rx[2] = {0., 0.};
