c857e8fe68c6d6b5cfb7b190eb99c857b9a03699
Bruno Benkel <bruno.benkel@gmail.com>
2023-02-01 12:07:48 -0300

    Added dumb bin tracking to draw_acc_corr

--- macros/draw_acc_corr.c
+++ macros/draw_acc_corr.c
@@ -17,7 +17,9 @@
 
 // --- Define macro constants here. ----------------------------------------- //
 // Set this to 1 to get some debug information.
-const int DEBUG = 0;
+const int DEBUG = 1;
+// Set to the PID to plot acceptance correction from.
+const int PID = -211;
 // acc_corr.txt produced by acc_corr.
 const char *INPUT_FILENAME  = "../data/acc_corr.txt";
 // Root file where we'll write the plots.
@@ -40,21 +42,31 @@ int main() {
     }
 
     // Read acceptance correction from file.
-    long int b_sizes[5];
+    long int bs[5];
     double **binnings;
     long int pids_size;
     long int nbins;
     long int *pids;
     double **acc_corr;
 
-    read_acc_corr_file(input_filename, b_sizes, &binnings, &pids_size, &nbins,
+    read_acc_corr_file(input_filename, bs, &binnings, &pids_size, &nbins,
             &pids, &acc_corr);
+
+    // Get place of PID in *pids.
+    int pid_pos = -1;
+    for (int pi = 0; pi < pids_size; ++pi) if (PID == pids[pi]) pid_pos = pi;
+    if (pid_pos == -1) {
+        printf("PID %d not found in %s! Exiting...\n", PID, input_filename);
+        return 1;
+    }
+
+    printf("pid_pos = %d\n", pid_pos);
 
     // Print read acceptance correction data for debugging purposes.
     if (DEBUG) {
         for (int bi = 0; bi < 5; ++bi) {
-            printf("binning[%02d] (%ld): [", bi, b_sizes[bi]);
-            for (int bii = 0; bii < b_sizes[bi]; ++bii)
+            printf("binning[%d] (%02ld): [", bi, bs[bi]);
+            for (int bii = 0; bii < bs[bi]; ++bii)
             printf("%5.2lf, ", binnings[bi][bii]);
             printf("\b\b]\n");
         }
@@ -66,6 +78,41 @@ int main() {
         }
         printf("\b\b]\n\n");
     }
+
+    // TODO. Plot.
+    int q2_bi = 0;
+    long int bin_size = bs[q2_bi];
+    // Define x[n] and y[n].
+    double x_pos[bin_size - 1];
+    double x_length[bin_size - 1];
+    double y[bin_size - 1];
+    for (int bii = 0; bii < bin_size - 1; ++bii) {
+        x_pos[bii]    = (binnings[q2_bi][bii+1] + binnings[q2_bi][bii])/2;
+        x_length[bii] = (binnings[q2_bi][bii+1] - binnings[q2_bi][bii])/2;
+        y[bii] = 0;
+    }
+
+    // Fill y.
+    for (int q2_i = 0; q2_i < bs[0]-1; ++q2_i) {
+        for (int nu_i = 0; nu_i < bs[1]-1; ++nu_i) {
+            for (int zh_i = 0; zh_i < bs[2]-1; ++zh_i) {
+                for (int pt2_i = 0; pt2_i < bs[3]-1; ++pt2_i) {
+                    for (int phiPQ_i = 0; phiPQ_i < bs[4]-1; ++phiPQ_i) {
+                        y[q2_i] += acc_corr[pid_pos][
+                            q2_i   * ((bs[1]-1)*(bs[2]-1)*(bs[3]-1)*(bs[4]-1)) +
+                            nu_i   * ((bs[2]-1)*(bs[3]-1)*(bs[4]-1)) +
+                            zh_i   * ((bs[3]-1)*(bs[4]-1)) +
+                            pt2_i  * ((bs[4]-1)) +
+                            phiPQ_i
+                        ];
+                    }
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < 2; ++i) printf("y[%d] = %8.5f\n", i, y[i]);
+    // for (int bi = 0; bi < 5; ++bi) {}
 
     // Clean up after ourselves.
     for (int bi = 0; bi < 5; ++bi) free(binnings[bi]);
