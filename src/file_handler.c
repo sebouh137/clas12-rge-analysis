b8ede14848641b93c315b905a75b930a26bf5440
Bruno Benkel <bruno.benkel@gmail.com>
2023-02-07 10:57:44 -0300

    Improved naming convention in file_handler

--- src/file_handler.c
+++ src/file_handler.c
@@ -42,26 +42,27 @@ int get_sf_params(char *filename, double sf[NSECTORS][SF_NPARAMS][2]) {
 }
 
 /**
- * Read binning data from text file and fill binning sizes array, binnings
+ * Read binning data from text file and fill binning sizes array, bin_edges
  *     array, and an array of PID list sizes.
  *
- * @param f_in:      file from which we extract the data.
- * @param b_sizes:   array of size 5 with binning sizes.
- * @param binnings:  2-dimensional array containing all 5 binnings.
- * @param pids_size: number of PIDs in list of PIDs.
- * @return:          success code (0).
+ * @param f_in:       file from which we extract the data.
+ * @param bin_nedges: array of size 5 with binning sizes.
+ * @param bin_edges:  2-dimensional array containing all 5 bin_edges.
+ * @param pids_size:  number of PIDs in list of PIDs.
+ * @return:           success code (0).
  */
-int get_binnings(FILE *f_in, long int *b_sizes, double **binnings,
+int get_bin_edges(FILE *f_in, long int *bin_nedges, double **bin_edges,
         long int *pids_size)
 {
     // Get binning sizes.
-    for (int bi = 0; bi < 5; ++bi) fscanf(f_in, "%ld ", &(b_sizes[bi]));
+    for (int bi = 0; bi < 5; ++bi) fscanf(f_in, "%ld ", &(bin_nedges[bi]));
 
-    // Get binnings.
+    // Get bin_edges.
     for (int bi = 0; bi < 5; ++bi) {
-        binnings[bi] = (double *) malloc(b_sizes[bi] * sizeof(*binnings[bi]));
-        for (int bii = 0; bii < b_sizes[bi]; ++bii)
-            fscanf(f_in, "%lf ", &(binnings[bi][bii]));
+        bin_edges[bi] =
+                (double *) malloc(bin_nedges[bi] * sizeof(*bin_edges[bi]));
+        for (int bii = 0; bii < bin_nedges[bi]; ++bii)
+            fscanf(f_in, "%lf ", &(bin_edges[bi][bii]));
     }
 
     // Get # of pids.
@@ -72,10 +73,10 @@ int get_binnings(FILE *f_in, long int *b_sizes, double **binnings,
 
 /**
  * Read acceptance correction data from text file and fill PIDs list and
- *     accceptance correction array. This function follows from get_binnings().
+ *     accceptance correction array. This function follows from get_bin_edges().
  *
  * @param f_in:      file from which we extract the data.
- * @param pids_size: number of PIDs to process, as found in get_binnings().
+ * @param pids_size: number of PIDs to process, as found in get_bin_edges().
  * @param nbins:     total number of bins.
  * @param pids:      array of PIDs to process.
  * @param n_thrown:  2-dimensional array containing the number of thrown events
@@ -116,9 +117,9 @@ int get_acc_corr(FILE *f_in, long int pids_size, long int nbins, long int *pids,
  *
  * @param acc_filename: char array with the filename containing the acceptance
  *                      correction to be processed.
- * @param b_sizes:      array that we'll fill with the size of each of the 5
- *                      binnings.
- * @param binnings:     pointer to a 2-dimensional array that we'll fill with
+ * @param bin_nedges:   array that we'll fill with the size of each of the 5
+ *                      bin_edges.
+ * @param bin_edges:    pointer to a 2-dimensional array that we'll fill with
  *                      the bin walls for each binning.
  * @param pids_size:    int where we'll write the number of PIDs in the file.
  * @param nbins:        int where we'll write the total number of bins.
@@ -132,21 +133,21 @@ int get_acc_corr(FILE *f_in, long int pids_size, long int nbins, long int *pids,
  *            * 0: Function performed correctly.
  *            * 1: Failed to access acceptance correction file.
  */
-int read_acc_corr_file(char *acc_filename, long int b_sizes[5],
-        double ***binnings, long int *pids_size, long int *nbins,
+int read_acc_corr_file(char *acc_filename, long int bin_nedges[5],
+        double ***bin_edges, long int *pids_size, long int *nbins,
         long int **pids, int ***n_thrown, int ***n_simul)
 {
     // Access file.
     if (access(acc_filename, F_OK) != 0) return 1;
     FILE *acc_file = fopen(acc_filename, "r");
 
-    // Get b_sizes, binnings, and pids_size.
-    *binnings = (double **) malloc(5 * sizeof(**binnings));
-    get_binnings(acc_file, b_sizes, *binnings, pids_size);
+    // Get bin_nedges, bin_edges, and pids_size.
+    *bin_edges = (double **) malloc(5 * sizeof(**bin_edges));
+    get_bin_edges(acc_file, bin_nedges, *bin_edges, pids_size);
 
     // Compute total number of bins.
     *nbins = 1;
-    for (int bi = 0; bi < 5; ++bi) *nbins *= b_sizes[bi] - 1;
+    for (int bi = 0; bi < 5; ++bi) *nbins *= (bin_nedges[bi] - 1);
 
     // Malloc list of pids and first dimension of pids and events.
     *pids = (long int *) malloc(*pids_size * sizeof(**pids));
